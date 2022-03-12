9e9f7ee3479ad8fc1c5711a26d58d51c2adf70f5
ridwanazeez <33818404+ridwanazeez@users.noreply.github.com>
2022-03-11 21:56:40 -0400

    add build folder

---
+++ assets/AboutView.c47c9edd.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.269aa46c.js";import{o as t,c as o,a as _}from"./vendor.88b7b7a5.js";const s={},a={class:"about"},c=_("h1",null,"This is an about page",-1),n=[c];function r(i,u){return t(),o("div",a,n)}var f=e(s,[["render",r]]);export{f as default};
