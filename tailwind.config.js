f77bbb7e7ebb9a5e8ad3505c10824ab6477b62c4
ridwanazeez <33818404+ridwanazeez@users.noreply.github.com>
2022-05-26 16:51:32 -0400

    adding missing dependencies for the UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
