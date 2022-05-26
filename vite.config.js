af32a20a5e68a8791d0c58a664e60bd5f15d8c08
ridwanazeez <33818404+ridwanazeez@users.noreply.github.com>
2022-05-26 18:07:08 -0400

    testing config change

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/ridwanazeez.github.io/",
   plugins: [vue()],
   resolve: {
     alias: {
