a84cc5b1b9183d23176c8f7fffa308bc08bf7963
ridwanazeez <33818404+ridwanazeez@users.noreply.github.com>
2022-05-27 00:55:23 -0400

    fixes

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import Home from "../views/Home.vue";
 import GRACalculator from "../views/GRACalculator.vue";
 import NotFound from "../views/404.vue";
