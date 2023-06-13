78fb36d73c9f4abb3febedb9511220ea9f206b00
Ridwan Azeez <ridwanazeez2000@gmail.com>
2023-06-13 09:24:39 -0400

    remove unused class

--- src/views/NumberChecker.vue
+++ src/views/NumberChecker.vue
@@ -40,7 +40,7 @@
       </TransitionChild>
 
       <div class="fixed z-10 inset-0 overflow-y-auto">
-        <div class="flex items-center sm:items-center justify-center min-h-full p-4 text-center sm:p-0">
+        <div class="flex items-center justify-center min-h-full p-4 text-center sm:p-0">
           <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" enter-to="opacity-100 translate-y-0 sm:scale-100" leave="ease-in duration-200" leave-from="opacity-100 translate-y-0 sm:scale-100" leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
             <DialogPanel class="relative bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-lg sm:w-full">
               <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
