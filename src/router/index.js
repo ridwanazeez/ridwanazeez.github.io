b63ea7170857fc47d8284720576c687c2142d240
Ridwan Azeez <ridwanazeez2000@gmail.com>
2024-05-18 21:15:10 -0400

    lazy redirect to ridwanazeez.com

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,19 @@ import { createRouter, createWebHistory } from "vue-router";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    // {
+    //   path: "/",
+    //   name: "home",
+    //   component: () => import("../views/Home.vue"),
+    //   meta: {
+    //     title: "Ridwan Azeez",
+    //   },
+    // },
     {
       path: "/",
       name: "home",
-      component: () => import("../views/Home.vue"),
-      meta: {
-        title: "Ridwan Azeez",
+      beforeEnter() {
+        window.location.href = "https://ridwanazeez.com";
       },
     },
     {
